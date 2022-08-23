dd752f88601fab9a77561888121ddbf223d69187
Ravi_Prakasha <46593559+Ravi-7093@users.noreply.github.com>
2022-08-23 16:22:31 -0500

    Update nr.py

--- List/First Non-Repeating Integer in a list/nr.py
+++ List/First Non-Repeating Integer in a list/nr.py
@@ -4,8 +4,8 @@ def find_first_unique(lst):
     for i in range(len(lst)):
         d[lst[i]]=d.get(lst[i],0)+1 #store the occurences of each character 
     print(d)
-    for key, value in d.items():#for each value in key,value in dictionary 
-        if value==1:#check if value ==1 
-            return key#return the key 
+    for key, value in d.items():#for each character in the dictionary 
+        if value==1:#check for the character having  value ==1 
+            return key#return the first charatcer satisfying the above condition
         
     
