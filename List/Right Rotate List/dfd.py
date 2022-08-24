6846fd65df3ae478d194ef0a55044c94102bcf1d
Ravi_Prakasha <46593559+Ravi-7093@users.noreply.github.com>
2022-08-24 17:17:14 -0500

    Update dfd.py

--- List/Right Rotate List/dfd.py
+++ List/Right Rotate List/dfd.py
@@ -1,13 +1,13 @@
 def right_rotate(lst, k):
     # Write your code here
-    if len(lst)==0:
+    if len(lst)==0:#check for length of the array
         k=0
     else:
-        k = k%len(lst)
+        k = k%len(lst) #check the value for k
     new_lst=[]
-    for i in range(len(lst)-k,len(lst)):
+    for i in range(len(lst)-k,len(lst)):#check for the start of the list
         print(i)
+        new_lst.append(lst[i])#append values for the new_lst
+    for i in range(0,len(lst)-k):#append the rest of the values to new_lst
         new_lst.append(lst[i])
-    for i in range(0,len(lst)-k):
-        new_lst.append(lst[i])
-    return new_lst
+    return new_lst#return the new_lst ie after right rotation 
